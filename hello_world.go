c5d0775daf348218ada00e01ac2b434d5489634d
LiuKang <583665989@qq.com>
2017-01-13 14:58:11 +0800

    add hello_world.go

---
+++ hello_world.go
@@ -0,0 +1,62 @@
+package main
+
+// 导包方式1
+//import "fmt"
+//import "math"
+// 导包方式2
+import (
+	"fmt"
+	"math"
+)
+//变量列表，后面bool为变量类型
+var a, b, c bool
+//包含初始值的变量定义，每个变量对应一个初始值
+var m, n int = 1, 2
+
+//程序入口
+func main() {
+	var i int
+	var x, y, z = true, false, "Hello"
+
+	s := 3  //短声明变量：使用 ":=" 替代 var 定义明确类型的变量.
+			//注意：只能在函数中使用，函数外只能使用 var
+
+
+	fmt.Println("Hello World")
+	fmt.Println(math.Pi)
+	fmt.Println(add(1, 2))
+	fmt.Println(adds(2, 3))
+	fmt.Println(swap("Hello", "World"))
+	fmt.Println(split(6))
+	fmt.Println(splits(6))
+	fmt.Println(i, a, b, c)
+	fmt.Println(m, n)
+	fmt.Println(x, y, z)
+	fmt.Println(s)
+}
+
+//函数（1）
+func add(x int, y int) int {
+	return x+y
+}
+//函数（2）
+func adds(x, y int) (int) {
+	return x + y
+}
+
+//多个返回值
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+//命名返回值
+func split(sum int) (x, y int) {
+	x = sum - 1
+	y = sum + 1
+	return
+}
+func splits(sum int) (x, y int) {
+	x = sum - 1
+	y = sum + 1
+	return y, x
+}
